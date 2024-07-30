a58d179eefae8e662757f25c38d847812514897f
Taiki Mizutani <taiki900824@gmail.com>
2024-07-31 08:25:09 +0900

    bird_eye_view_pointselection

--- bird_eye_view_pointSelection.cpp
+++ bird_eye_view_pointSelection.cpp
@@ -33,7 +33,12 @@ public:
         cv::namedWindow(windowName);
         cv::setMouseCallback(windowName, mouseCallback);
 
-        std::cout << "Please select 4 points on the image." << std::endl;
+        std::cout << "Please select 4 points on the image in the following order:" << std::endl;
+        std::cout << "1. Top-left corner of the lane" << std::endl;
+        std::cout << "2. Top-right corner of the lane" << std::endl;
+        std::cout << "3. Bottom-left corner of the lane" << std::endl;
+        std::cout << "4. Bottom-right corner of the lane" << std::endl;
+
         cv::imshow(windowName, img);
         cv::waitKey(0);
 
@@ -56,7 +61,21 @@ public:
     Camera(int width, int height) : img_width(width), img_height(height) {}
 
     void calculateTransform(const std::vector<cv::Point>& lane_shape) {
-        // ... (前のコードと同じ)
+        // 選択された点の順序: [top-left, top-right, bottom-left, bottom-right]
+        cv::Point2f top_left(lane_shape[0]);
+        cv::Point2f top_right(lane_shape[1]);
+        cv::Point2f bottom_left(lane_shape[2]);
+        cv::Point2f bottom_right(lane_shape[3]);
+
+        std::vector<cv::Point2f> source = {top_left, top_right, bottom_right, bottom_left};
+        std::vector<cv::Point2f> destination = {
+            cv::Point2f(300, 200),
+            cv::Point2f(500, 200),
+            cv::Point2f(500, 400),
+            cv::Point2f(300, 400)
+        };
+
+        overhead_transform = cv::getPerspectiveTransform(source, destination);
     }
 
     cv::Mat warp_to_overhead(const cv::Mat& undistorted_img, const std::string& save_path = "") {
@@ -77,7 +96,7 @@ public:
 };
 
 int main() {
-    std::string imagePath = "/home/tki256/myProject/opencv-class/img/view1.jpg";  // 入力画像のパスを指定してください
+    std::string imagePath = "/home/tki256/myProject/opencv-class/img-out/view4.jpg";  // 入力画像のパスを指定してください
     cv::Mat image = cv::imread(imagePath);
     resize(image, image, cv::Size(), 0.2, 0.2);
     if (image.empty()) {
@@ -90,7 +109,7 @@ int main() {
     Camera camera(image.cols, image.rows);
     camera.calculateTransform(selectedPoints);
 
-    cv::Mat overhead = camera.warp_to_overhead(image, "overhead_view.jpg");
+    cv::Mat overhead = camera.warp_to_overhead(image, "/home/tki256/myProject/opencv-class/result/overhead_view4.jpg");
 
     cv::imshow("Overhead View", overhead);
     cv::waitKey(0);
